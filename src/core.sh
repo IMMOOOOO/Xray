b91efd30d31bdd0a4027813ec909e3eaa9a1a72a
IMMOOO <127593751+IMMOOOOO@users.noreply.github.com>
2026-06-30 00:48:24 +0800

    disable sniffing for dokodemo-door

--- src/core.sh
+++ src/core.sh
@@ -349,6 +349,7 @@ create() {
         is_listen='listen:"0.0.0.0"'
         [[ $host ]] && is_listen=${is_listen/0.0.0.0/127.0.0.1}
         is_sniffing='sniffing:{enabled:true,destOverride:["http","tls"]}'
+        [[ $is_protocol == 'dokodemo-door' ]] && is_sniffing='sniffing:{enabled:false}'
         [[ $is_reality ]] && is_sniffing='sniffing:{enabled:true,destOverride:["http","tls"],routeOnly:true}'
         is_new_json=$(jq '{inbounds:[{tag:"'$is_config_name'",port:'"$port"','"$is_listen"',protocol:"'$is_protocol'",'"$json_str"','"$is_sniffing"'}]}' <<<{})
         if [[ $is_dynamic_port ]]; then
